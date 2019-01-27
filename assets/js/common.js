f0d9497846f9b78f75921aabfbccc2b9470ed824
Roman <texxnic@gmail.com>
2019-01-27 21:07:38 +0300

    27.01

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,42 +1,74 @@
 window.onscroll = () => {
     const nav = document.querySelector('.headerMenu');
-    if(this.scrollY <= 10) nav.className = 'headerMenu'; else nav.className = 'headerMenu headerScroll';
-  };
+    if (this.scrollY <= 10) nav.className = 'headerMenu'; else nav.className = 'headerMenu headerScroll';
+};
 
-  var controller = new ScrollMagic.Controller();
-  
-  new ScrollMagic.Scene({
-      duration: $(window).height(),
-      triggerHook: 0,
+var controller = new ScrollMagic.Controller();
+
+new ScrollMagic.Scene({
+    duration: $(window).height(),
+    triggerHook: 0,
+
+})
+    .setTween(".jumbo_png", { top: '-20%' })
+    /* .addIndicators() */
+    .addTo(controller);
+/* Прозрачность при скролле */
+new ScrollMagic.Scene({
+    duration: $(window).height() / 2,
+    triggerHook: 0,
+
+})
+    .setTween(".jumbo", { opacity: 0 })
+    /* .addIndicators() */
+    .addTo(controller);
+$(".jumboText").mousemove(function (e) {
+    
+    parallaxIt(e, ".jumbo_png", -30);
+    //   parallaxIt(e, ".jumbo__logo", -45);
+    parallaxIt(e, ".jumbo__image", -70);
+});
+
+function parallaxIt(e, target, movement) {
+    var $this = $(".jumbo");
+    var relX = e.pageX - $this.offset().left;
+    var relY = e.pageY - $this.offset().top;
+
+    TweenMax.to(target, 1, {
+        x: (relX - $this.width() / 2) / $this.width() * movement,
+        y: (relY - $this.height() / 2) / $this.height() * movement
+    });
+
+}
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        onChange: owlChanged,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed:2000,
+    });
+    var el = document.querySelector('.aboutCompanyGhost');
+    od = new Odometer({
+        el: el,
+        value: 20,
       
-  })
-      .setTween(".jumbo_png", { top: '-20%'})
-      /* .addIndicators() */
-      .addTo(controller);
-      /* Прозрачность при скролле */
-  new ScrollMagic.Scene({
-      duration: $(window).height()/2,
-      triggerHook: 0,
-      
-  })
-  .setTween(".jumbo", { opacity: 0 })
-      /* .addIndicators() */
-      .addTo(controller);
-  $(".jumboText").mousemove(function (e) {
-      console.log(e);
-      parallaxIt(e, ".jumbo_png", -30);
-      // parallaxIt(e, ".jumboText", -45);
-      parallaxIt(e, ".jumbo__image", -70);
-  });
-
-  function parallaxIt(e, target, movement) {
-      var $this = $(".jumbo");
-      var relX = e.pageX - $this.offset().left;
-      var relY = e.pageY - $this.offset().top;
-
-      TweenMax.to(target, 1, {
-          x: (relX - $this.width() / 2) / $this.width() * movement,
-          y: (relY - $this.height() / 2) / $this.height() * movement
+        
+        
       });
-
-  }
+    function owlChanged(event){
+        switch (event.item.index) {
+            case 1:
+            od.update(20);
+              break;
+            case 2:
+              od.update(15);
+              break;
+            case 3:
+            od.update(60);
+              break;
+          
+          }
+    };
+});
